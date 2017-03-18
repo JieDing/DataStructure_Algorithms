dee60de022057713f55a4c195ea8c0b29e0ef23f
JieDing <dingjwilliams@163.com>
2017-03-19 00:47:43 +0800

    firstCommit

---
+++ DataStructureAndAlgorithm/src/test/java/com/jieding/test/MyArrayListTest.java
@@ -0,0 +1,14 @@
+package com.jieding.test;
+
+import junit.framework.TestCase;
+
+public class MyArrayListTest extends TestCase {
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	public void testClear() {
+		System.out.println("asdasdadasd");
+	}
+
+}
