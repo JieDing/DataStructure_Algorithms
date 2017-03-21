fe664f7204cb0c04d26cb62aadcb3275582b5401
JieDing <dingjwilliams@gmail.com>
2017-03-21 21:56:35 +0800

    firstCommit

---
+++ DataStructureAndAlgorithm/src/main/java/com/jieding/datastructure/MyDoublyLinkedQueue.java
@@ -0,0 +1,103 @@
+package com.jieding.datastructure;
+
+import com.jieding.adt.QueueADT;
+
+public class MyDoublyLinkedQueue<E> implements QueueADT<E> {
+	
+	private Node headNode;
+	private Node tailNode;
+	private int size;
+	
+	public MyDoublyLinkedQueue(){
+		headNode = new Node(null,null,null);
+		tailNode = new Node(headNode,null,null);
+		headNode.next = tailNode;
+		this.size = 0;
+	}
+	
+	public void clear() {
+		// TODO Auto-generated method stub
+		for(Node n=headNode; n!=null;){
+			Node next = n.next;
+			n.prev = null;
+			n.next = null;
+			n.value = null;
+			n = next;
+		}
+		headNode = null;
+		tailNode = null;
+		size = 0;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return this.size;
+	}
+
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return size()==0;
+	}
+
+	public boolean contains(E e) {
+		// TODO Auto-generated method stub
+		if(isEmpty()) return false;
+		if(e==null) throw new IllegalArgumentException("please enter non-null element");
+		for(Node n=headNode.next; n.next!=null; n=n.next){
+			if(e.equals(n.value))
+				return true;
+		}
+		return false;
+	}
+
+	public E peek() {
+		// TODO Auto-generated method stub
+		if(isEmpty()) return null;
+		return headNode.next.value;
+	}
+	/**
+	 * return 1-based index of the specific element
+	 * return 0 if the queue is empty
+	 * return -1 if the element is not existed
+	 */
+	public int search(E e) {
+		// TODO Auto-generated method stub
+		if(isEmpty()) return 0;
+		int i=1;
+		for(Node n=headNode.next; n.next!=null; n=n.next, i++){
+			if(e.equals(n.value))
+				return i;
+		}
+		return -1;
+	}
+
+	public boolean enqueue(E e) {
+		// TODO Auto-generated method stub
+		Node n = new Node(tailNode.prev, tailNode, e);
+		tailNode.prev.next = n;
+		tailNode.prev = n;
+		size++;
+		return true;
+	}
+
+	public E dequeue() {
+		// TODO Auto-generated method stub
+		if(isEmpty()) return null;
+		Node firstNode = headNode.next;
+		E value = firstNode.value;
+		firstNode.next.prev = headNode;
+		headNode.next = firstNode.next;
+		size--;
+		return value;
+	}
+	class Node{
+		Node prev;
+		Node next;
+		E value;
+		Node (Node prev, Node next, E value){
+			this.prev = prev;
+			this.next = next;
+			this.value = value;
+		}
+	}
+}
---
+++ DataStructureAndAlgorithm/src/test/java/com/jieding/test/MyDoublyLinkedQueueTest.java
@@ -0,0 +1,120 @@
+package com.jieding.test;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.jieding.adt.QueueADT;
+import com.jieding.datastructure.MyDoublyLinkedQueue;
+
+public class MyDoublyLinkedQueueTest extends TestCase {
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	public void testClear() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		q.clear();
+		Assert.assertEquals(0, q.size());
+		Assert.assertEquals(null, q.peek());
+	}
+
+	public void testSize() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		q.enqueue("cde");
+		q.enqueue("def");
+		q.dequeue();
+		q.dequeue();
+		q.dequeue();
+		q.dequeue();
+		Assert.assertEquals(0, q.size());
+
+	}
+
+	public void testIsEmpty() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		Assert.assertEquals(false, q.isEmpty());
+	}
+
+	public void testContains() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		q.enqueue("cde");
+		q.enqueue("def");
+		Assert.assertEquals(true, q.contains("abc"));
+		q.dequeue();
+		Assert.assertEquals(false, q.contains("abc"));
+		Assert.assertEquals("bcd", q.peek());
+	}
+
+	public void testPeek() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		q.enqueue("cde");
+		q.enqueue("def");
+		Assert.assertEquals("abc", q.peek());
+		q.dequeue();
+		Assert.assertEquals("bcd", q.peek());
+	}
+
+	public void testSearch() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		q.enqueue("cde");
+		q.enqueue("def");
+		Assert.assertEquals(1, q.search("abc"));
+		Assert.assertEquals(3, q.search("cde"));
+		q.dequeue();
+		Assert.assertEquals(-1, q.search("abc"));
+		Assert.assertEquals(2, q.search("cde"));
+	}
+
+	public void testEnqueue() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		q.enqueue("cde");
+		q.enqueue("def");
+		Assert.assertEquals(1, q.search("abc"));
+		Assert.assertEquals(3, q.search("cde"));
+		q.dequeue();
+		Assert.assertEquals(-1, q.search("abc"));
+		Assert.assertEquals(2, q.search("cde"));
+		Assert.assertEquals(true, q.contains("bcd"));
+		Assert.assertEquals("bcd", q.peek());
+		q.dequeue();
+		q.dequeue();
+		q.dequeue();
+		Assert.assertEquals(null, q.peek());
+		Assert.assertEquals(0, q.size());
+	}
+
+	public void testDequeue() {
+		QueueADT<String> q = new MyDoublyLinkedQueue<String>();
+		q.enqueue("abc");
+		q.enqueue("bcd");
+		q.enqueue("cde");
+		q.enqueue("def");
+		Assert.assertEquals(1, q.search("abc"));
+		Assert.assertEquals(3, q.search("cde"));
+		q.dequeue();
+		Assert.assertEquals(-1, q.search("abc"));
+		Assert.assertEquals(2, q.search("cde"));
+		Assert.assertEquals(true, q.contains("bcd"));
+		Assert.assertEquals("bcd", q.peek());
+		q.dequeue();
+		q.dequeue();
+		q.dequeue();
+		Assert.assertEquals(null, q.peek());
+		Assert.assertEquals(0, q.size());
+	}
+
+}
